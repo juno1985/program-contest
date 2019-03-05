4b101d7471e11895c5c046a4e955202c2aae181d
Jun <wangzhen_tju@126.com>
2019-03-06 07:39:37 +0800

    增加model case表

--- db.sql
+++ db.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS `t_problem_difficulty`;
 DROP TABLE IF EXISTS `t_problem_type`;
 DROP TABLE IF EXISTS `t_problems`;
+DROP TABLE IF EXISTS `t_problem_cases`;
+
 
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
@@ -38,3 +40,14 @@ CREATE TABLE `t_problem_difficulty`(
 ALTER TABLE `t_problems` AUTO_INCREMENT = 1;
 ALTER TABLE `t_problem_type` AUTO_INCREMENT = 1;
 ALTER TABLE `t_problem_difficulty` AUTO_INCREMENT = 1;
+
+CREATE TABLE `t_problem_cases`(
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`input` TEXT COLLATE utf8_bin,
+	`output` TEXT COLLATE utf8_bin,
+	`fid` INT(11),
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`fid`) REFERENCES `t_problems` (`id`)
+	
+)ENGINE=INNODB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
