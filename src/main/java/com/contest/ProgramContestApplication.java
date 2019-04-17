5475ed2b88fa4cc5f54de15526d02a793e879638
Juno1985 <34500985+juno1985@users.noreply.github.com>
2019-04-18 06:46:13 +0800

    打war

--- src/main/java/com/contest/ProgramContestApplication.java
+++ src/main/java/com/contest/ProgramContestApplication.java
@@ -3,15 +3,22 @@ package com.contest;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan("com.contest.mapper")
 @ComponentScan("com.contest")
 @SpringBootApplication
-public class ProgramContestApplication {
+public class ProgramContestApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgramContestApplication.class, args);
 	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(ProgramContestApplication.class);
+	    }
 
 }
